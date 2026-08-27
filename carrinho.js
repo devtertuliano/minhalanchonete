be67b14b43d35af46b45f3b8980a37091b01680c
Julio Junior <dev.tertuliano@gmail.com>
2026-08-27 10:59:09 -0300

    feito botao de finalizar e remover item do carrinho, e atualização do CSS para deixar o carrinho na lateral e correção de sintaxes do html com css que tinha tag que nao existia.

--- carrinho.js
+++ carrinho.js
@@ -1,26 +1,52 @@
-const carrinho = [];
+let dadosSalvos = localStorage.getItem("carrinho");
+const carrinho = JSON.parse(dadosSalvos) || [];
 
 const botoes = document.querySelectorAll(".btn-adicionar");
 
-botoes.forEach(function(botao){
-    botao.addEventListener('click', function(event) {
+botoes.forEach(function (botao) {
+  botao.addEventListener("click", function (event) {
     let preco = event.target.dataset.preco;
     let nome = event.target.dataset.nome;
     let precoConvertido = Number(preco);
-    carrinho.push({nome: nome, preco: precoConvertido});
+    carrinho.push({ nome: nome, preco: precoConvertido });
     atualizarCarrinhoNaTela();
-    }); 
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  });
 });
 
 function atualizarCarrinhoNaTela() {
   let container = document.getElementById("container-Carrinho");
-   let html = "";
-   let total = 0;
-   
- carrinho.forEach(function(item) {
-    html += `<p>${item.nome} - R$ ${item.preco}</p>`;
-    total += item.preco;
-  });
-  html += `<p><strong>Total: R$ ${total.toFixed(2)}</strong></p>`
+  let html = "";
+  let total = 0;
+  if (carrinho.length === 0) {
+    html = "<p>O carrinho está vazio.</p>";
+  } else {
+    carrinho.forEach(function (item, index) {
+      html += `<p>${item.nome} - R$ ${item.preco} <button class="btn-remover" data-index="${index}">remover</button></p>`;
+      total += item.preco;
+    });
+    html += `<p><strong>Total: R$ ${total.toFixed(2)}</strong></p>`;
+    html += `<button class="btn-finalizar">Finalizar Pedido</button>`;
+  }
   container.innerHTML = html;
 }
+
+atualizarCarrinhoNaTela();
+
+let containerCarrinho = document.getElementById("container-Carrinho");
+
+containerCarrinho.addEventListener("click", function (event) {
+  let index = event.target.classList.contains("btn-remover")
+    ? event.target.dataset.index
+    : null;
+  if (index !== null) {
+    carrinho.splice(Number(index), 1);
+    atualizarCarrinhoNaTela();
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }else if (event.target.classList.contains("btn-finalizar")) {
+    alert("Pedido finalizado com sucesso!");
+    carrinho.length = 0;
+    atualizarCarrinhoNaTela();
+    localStorage.removeItem("carrinho");
+  }
+});
